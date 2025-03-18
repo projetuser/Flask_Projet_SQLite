951fb790abf9aabe44306aaaa320b4ec98593218
Sushi <71588514+projetuser@users.noreply.github.com>
2025-03-18 14:29:04 +0100

    Update Biblioteque.py

--- Biblioteque.py
+++ Biblioteque.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 
-with open('schema.sql') as f:
+with open('Creation_tableau.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
