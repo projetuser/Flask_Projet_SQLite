3197df0cc45f480149fed657164373259e6cdc78
Sushi <71588514+projetuser@users.noreply.github.com>
2025-03-18 15:20:49 +0100

    Update Creation_tableau.sql

--- Creation_tableau.sql
+++ Creation_tableau.sql
@@ -1,7 +1,9 @@
+-- Suppression des tables existantes si elles existent
 DROP TABLE IF EXISTS utilisateurs;
 DROP TABLE IF EXISTS livres;
 DROP TABLE IF EXISTS emprunts;
 
+-- Création de la table des utilisateurs
 CREATE TABLE utilisateurs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
@@ -9,20 +11,20 @@ CREATE TABLE utilisateurs (
     role TEXT CHECK(role IN ('admin', 'user')) NOT NULL
 );
 
+-- Création de la table des livres
 CREATE TABLE livres (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     titre TEXT NOT NULL,
     auteur TEXT NOT NULL,
-    disponible INTEGER NOT NULL DEFAULT 1,
-    nombre_exemplaires INTEGER NOT NULL DEFAULT 1  -- Ajout du champ nombre_exemplaires
+    disponible INTEGER NOT NULL DEFAULT 1
 );
 
+-- Création de la table des emprunts
 CREATE TABLE emprunts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     livre_id INTEGER NOT NULL,
     date_emprunt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    date_retour TIMESTAMP,  -- Ajout de la date de retour
     FOREIGN KEY (user_id) REFERENCES utilisateurs(id),
     FOREIGN KEY (livre_id) REFERENCES livres(id)
 );
